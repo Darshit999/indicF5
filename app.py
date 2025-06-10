36ad9e3bf1b8f832ff3a5c1490112b4b209e2e7e
DarshitDobariya <96968437+Darshit999@users.noreply.github.com>
2025-06-10 21:36:38 +0530

    fixes

--- app.py
+++ app.py
@@ -96,11 +96,71 @@ EXAMPLES = [
         "ref_text": "क्या हाल है चाई का कप उठाओ और बैठ कर सुनो बातें जब इस आवाज में होंगी तो सुकून सा लगेगा"
     },
     {
+        "label": "Anika",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/female/anika.mp3",
+        "ref_text": " छोटी गुडिया ने जैसे ही जादूई दर्वाजा खोला, एक चमकदार दुनिया उसके सामने थी। उसने कहा, क्या यहां सच में बोलने वाले खरगोश हैं।"
+    },
+    {
+        "label": "Diya",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/female/diya.mp3",
+        "ref_text": " जो कहानी मैं सुनाने वाली हूँ, उसकी कोई तस्वीर नहीं, कोई गवाह नहीं, सिर्फ एक डर, जो अब तक मेरे अंदर जिन्दा है।"
+    },
+    {
+        "label": "Jessica",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/female/jessica.mp3",
+        "ref_text": "जो व्यक्ति चक्कर खा रहा होता है, वह समझता है कि दुनिया घूम रही है।"
+    },
+    {
+        "label": "Monika",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/female/monika.mp3",
+        "ref_text": "ईलेवन लेब्स की सबसे सफल भारतिय आवाज अब हिंदी में, तो आईए कुछ बहतरीन बनाते हैं।"
+    },
+    {
+        "label": "Muskaan",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/female/muskaan.mp3",
+        "ref_text": "अरे बैठो न, आराम से कहानी सुनते हैं, बातों बातों में सब समझ आ जाएगा"
+    },
+    {
+        "label": "Meher",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/female/meher.mp3",
+        "ref_text": "वो दर्वाजा धीरे धीरे खुला. अंदर अंधेरा था. लेकिन अजीब ये था कि किसी ने दर्वाजा खोला नहीं था."
+    },
+    
+    {
+        "label": "Kunal",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/male/kunal.mp3",
+        "ref_text": "अगर आप उस समय मुस्कुराते हैं जब आपके आसपास कोई नहीं होता, तो आप वास्तव में इसका मतलब समझते हैं।"
+    },
+    {
+        "label": "Yatin",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/male/yatin.mp3",
+        "ref_text": "एक गहरा रहस्य हवा मै तैर रहा था, हर सांस के साथ करीब आता हुआ, और किसी को नही पता था की अगली पल क्या लेकर आएगा।"
+    },
+    {
+        "label": "Neeraj",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/male/neeraj.wav",
+        "ref_text": "भारतिय जीवन धारा में जिन ग्रंथों का महत्व पूर्ण स्थान है। उनमें पुरान भक्ती ग्रंथों के रूप में बहुत महत्व पूर्ण माने जाते हैं।"
+    },
+    {
+        "label": "Rudra",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/male/rudra.mp3",
+        "ref_text": "कितनी अजीब बात है कि आजकल लोगों ने किताबें पढ़ना छोड़ ही दिया है। अब किताबें अगर सुननी ही हैं, तो अच्छी आवाज में सुनो।"
+    },
+    {
+        "label": "Viraj",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/male/viraj.mp3",
+        "ref_text": "कुछ तो है जो छिपा हुआ है। हवा में हलकी सी गड़-गड़ा हट जैसे कोई राज खुलने वाला हो। क्या आपने वो महसूस किया?"
+    },
+    {
+        "label": "Vikas",
+        "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/male/vikas.mp3",
+        "ref_text": "अरे हा, मुझे वो दिन अच्छी तरह याद है, सब लोग इतने खुश थे और माहोल भी बहोत खास था।"
+    },
+    {
         "label": "Arjun",
         "audio_url": "https://github.com/Darshit999/indicF5/raw/main/voices/male/arjun.mp3",
         "ref_text": "आपको परिवर्तन को नियम के रूप में स्वागत करना चाहिए, लेकिन अपने शासक के रूप में नहीं."
     },
-    # Add more examples if needed...
 ]
 
 for ex in EXAMPLES:
